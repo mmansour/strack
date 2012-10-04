42864d76699f52dac0826c4d04cf683e8c50d6f9
Matt Mansour <slackbabbath@gmail.com>
2012-10-04 14:05:23 -0700

    Adding new twitter and facebook updating things

---
+++ trackit/management/commands/get_facebook.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from BeautifulSoup import BeautifulSoup
+from trackit.models import Youtuber
+import urllib2
+import time
+
+class Command(BaseCommand):
+    help = 'Get all facebook likes'
+    def handle(self, *args, **options):
+
+        yt = Youtuber.objects.exclude(facebook_url__isnull=True).exclude(facebook_url__exact='')
+
+        for user in yt:
+            time.sleep(1)
+            fburldb = Youtuber.objects.get(title=user.title)
+            fbpage=urllib2.urlopen(user.facebook_url)
+            fbsoup=BeautifulSoup(fbpage.read())
+            fbsociallink=fbsoup.findAll('meta',{'name':'description'})
+            fbdigitlist = [s for s in fbsociallink[0]['content'].split()
+                                if s.replace(",","").isdigit()]
+#                        print k, fbdigitlist, fbsociallink[0]['content']
+            fburldb.facebook_likes = fbdigitlist[0]
+            fburldb.save()
+##            print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], user.facebook_url)
+
+
---
+++ trackit/management/commands/get_twitter.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from BeautifulSoup import BeautifulSoup
+from trackit.models import Youtuber
+import urllib2
+
+
+class Command(BaseCommand):
+    help = 'Get all Twitter Followers'
+    def handle(self, *args, **options):
+
+        yt = Youtuber.objects.exclude(twitter_url__isnull=True).exclude(twitter_url__exact='')
+
+        for user in yt:
+            twitterdb = Youtuber.objects.get(title=user.title)
+            twiturl = user.twitter_url
+            twitcleanurl = twiturl.replace('www.','').replace('#!/','')
+            twitpage=urllib2.urlopen(twitcleanurl)
+            twitsoup=BeautifulSoup(twitpage.read())
+            twitsocialdata=twitsoup.findAll('a',{'data-nav':'followers'})
+#               print k, twitsociallink[0].findAll(text=True)[1], twitsociallink[0].findAll(text=True)[2]
+            twitfollowers = [f.findAll(text=True) for f in twitsocialdata]
+            twitsorteddata = sorted(twitfollowers)
+            twitterdb.twitter_followers = twitsorteddata[0][1]
+            twitterdb.save()
+#            print 'User: {0}, Followers: {1} Url: {2}'.format(user.title, twitsorteddata[0][1], user.twitter_url)
+
+
