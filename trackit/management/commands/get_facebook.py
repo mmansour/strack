56cff5e520766ff3e42e13ffa87322a8d28d17df
Matt Mansour <slackbabbath@gmail.com>
2012-10-04 17:38:26 -0700

    Removing errors

--- trackit/management/commands/get_facebook.py
+++ trackit/management/commands/get_facebook.py
@@ -8,28 +8,28 @@ class Command(BaseCommand):
     help = 'Get all facebook likes'
     def handle(self, *args, **options):
 
-        yt = Youtuber.objects.exclude(facebook_url__isnull=True)\
-            .exclude(facebook_url__exact='')\
-            .exclude(facebook_error__isnull=False)
+        yt = Youtuber.objects.exclude(facebook_url__isnull=True).exclude(facebook_url__exact='')
 
         for user in yt:
             time.sleep(1)
-            if user.facebook_url[:4] != 'http':
-                clean_url = 'http://{0}'.format(user.facebook_url)
-            else:
-                clean_url = user.facebook_url
+            if not user.facebook_error:
+                if user.facebook_url[:4] != 'http':
+                    clean_url = 'http://{0}'.format(user.facebook_url)
+                else:
+                    clean_url = user.facebook_url
 
-            fburldb = Youtuber.objects.get(title=user.title)
-            fbpage=urllib2.urlopen(clean_url)
-            fbsoup=BeautifulSoup(fbpage.read())
-            fbsociallink=fbsoup.findAll('meta',{'name':'description'})
-            fbdigitlist = [s for s in fbsociallink[0]['content'].split()
-                                if s.replace(",","").isdigit()]
-#                        print k, fbdigitlist, fbsociallink[0]['content']
-            fburldb.facebook_likes = fbdigitlist[0]
-            fburldb.save()
+                fburldb = Youtuber.objects.get(title=user.title)
+                fbpage=urllib2.urlopen(clean_url)
+                fbsoup=BeautifulSoup(fbpage.read())
+                fbsociallink=fbsoup.findAll('meta',{'name':'description'})
+                fbdigitlist = [s for s in fbsociallink[0]['content'].split()
+                                    if s.replace(",","").isdigit()]
+    #                        print k, fbdigitlist, fbsociallink[0]['content']
+                fburldb.facebook_likes = fbdigitlist[0]
+                fburldb.save()
 
-            print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], clean_url)
+                print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], clean_url)
+                
         print 'Done pulling Facebook Likes'
 
 
--- trackit/management/commands/get_twitter.py
+++ trackit/management/commands/get_twitter.py
@@ -9,28 +9,28 @@ class Command(BaseCommand):
     help = 'Get all Twitter Followers'
     def handle(self, *args, **options):
 
-        yt = Youtuber.objects.exclude(twitter_url__isnull=True)\
-            .exclude(twitter_url__exact='')\
-            .exclude(twitter_error__isnull=False)
+        yt = Youtuber.objects.exclude(twitter_url__isnull=True).exclude(twitter_url__exact='')
 
         for user in yt:
             time.sleep(1)
-            twitterdb = Youtuber.objects.get(title=user.title)
-            twiturl = user.twitter_url
-            twitcleanurl = twiturl.replace('www.','').replace('#!/','')
+            if not user.twitter_error:
+                twitterdb = Youtuber.objects.get(title=user.title)
+                twiturl = user.twitter_url
+                twitcleanurl = twiturl.replace('www.','').replace('#!/','')
 
-            if twitcleanurl[:4] != 'http':
-                twitcleanurl = 'http://{0}'.format(twitcleanurl)
+                if twitcleanurl[:4] != 'http':
+                    twitcleanurl = 'http://{0}'.format(twitcleanurl)
 
-            twitpage=urllib2.urlopen(twitcleanurl)
-            twitsoup=BeautifulSoup(twitpage.read())
-            twitsocialdata=twitsoup.findAll('a',{'data-nav':'followers'})
-#               print k, twitsociallink[0].findAll(text=True)[1], twitsociallink[0].findAll(text=True)[2]
-            twitfollowers = [f.findAll(text=True) for f in twitsocialdata]
-            twitsorteddata = sorted(twitfollowers)
-            twitterdb.twitter_followers = twitsorteddata[0][1]
-            twitterdb.save()
-            
-            print 'User: {0}, Followers: {1} Url: {2}'.format(user.title, twitsorteddata[0][1], twitcleanurl)
+                twitpage=urllib2.urlopen(twitcleanurl)
+                twitsoup=BeautifulSoup(twitpage.read())
+                twitsocialdata=twitsoup.findAll('a',{'data-nav':'followers'})
+#                print k, twitsociallink[0].findAll(text=True)[1], twitsociallink[0].findAll(text=True)[2]
+                twitfollowers = [f.findAll(text=True) for f in twitsocialdata]
+                twitsorteddata = sorted(twitfollowers)
+                twitterdb.twitter_followers = twitsorteddata[0][1]
+                twitterdb.save()
+
+                print 'User: {0}, Followers: {1} Url: {2}'.format(user.title, twitsorteddata[0][1], twitcleanurl)
+                
         print 'Done Pulling Twitter Followers'
 
