e4fb8f740af5ecefa800851a068f0396050ec9c1
Matt Mansour <slackbabbath@gmail.com>
2012-10-04 17:44:56 -0700

    Adding err trap to fblikes

--- trackit/management/commands/get_facebook.py
+++ trackit/management/commands/get_facebook.py
@@ -13,23 +13,27 @@ class Command(BaseCommand):
         for user in yt:
             time.sleep(1)
             if not user.facebook_error:
-                if user.facebook_url[:4] != 'http':
-                    clean_url = 'http://{0}'.format(user.facebook_url)
-                else:
-                    clean_url = user.facebook_url
-
                 fburldb = Youtuber.objects.get(title=user.title)
-                fbpage=urllib2.urlopen(clean_url)
-                fbsoup=BeautifulSoup(fbpage.read())
-                fbsociallink=fbsoup.findAll('meta',{'name':'description'})
-                fbdigitlist = [s for s in fbsociallink[0]['content'].split()
-                                    if s.replace(",","").isdigit()]
-    #                        print k, fbdigitlist, fbsociallink[0]['content']
-                fburldb.facebook_likes = fbdigitlist[0]
-                fburldb.save()
+                try:
+                    if user.facebook_url[:4] != 'http':
+                        clean_url = 'http://{0}'.format(user.facebook_url)
+                    else:
+                        clean_url = user.facebook_url
 
-                print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], clean_url)
-                
+                    fbpage=urllib2.urlopen(clean_url)
+                    fbsoup=BeautifulSoup(fbpage.read())
+                    fbsociallink=fbsoup.findAll('meta',{'name':'description'})
+                    fbdigitlist = [s for s in fbsociallink[0]['content'].split()
+                                        if s.replace(",","").isdigit()]
+        #                        print k, fbdigitlist, fbsociallink[0]['content']
+                    fburldb.facebook_likes = fbdigitlist[0]
+                    fburldb.save()
+                    print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], clean_url)
+                except IndexError, e:
+                    fburldb.facebook_error = 'Missing Likes: {0}'.format(e)
+                    fburldb.save()
+                    print 'User: {0}, Missing Likes: {1}'.format(user.title, e)
+
         print 'Done pulling Facebook Likes'
 
 
--- trackit/management/commands/get_twitter.py
+++ trackit/management/commands/get_twitter.py
@@ -14,6 +14,7 @@ class Command(BaseCommand):
         for user in yt:
             time.sleep(1)
             if not user.twitter_error:
+
                 twitterdb = Youtuber.objects.get(title=user.title)
                 twiturl = user.twitter_url
                 twitcleanurl = twiturl.replace('www.','').replace('#!/','')
