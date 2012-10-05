23c5f9dd62cd516f934a7df1fc77c625e497e0ed
Matt Mansour <slackbabbath@gmail.com>
2012-10-04 18:22:09 -0700

    Fixing errors

--- trackit/management/commands/get_twitter.py
+++ trackit/management/commands/get_twitter.py
@@ -13,24 +13,33 @@ class Command(BaseCommand):
 
         for user in yt:
             time.sleep(1)
-
             twitterdb = Youtuber.objects.get(title=user.title)
-            twiturl = user.twitter_url
-            twitcleanurl = twiturl.replace('www.','').replace('#!/','')
+            try:
+                twiturl = user.twitter_url
+                twitcleanurl = twiturl.replace('www.','').replace('#!/','')
 
-            if twitcleanurl[:4] != 'http':
-                twitcleanurl = 'http://{0}'.format(twitcleanurl)
+                if twitcleanurl[:4] != 'http':
+                    twitcleanurl = 'http://{0}'.format(twitcleanurl)
 
-            twitpage=urllib2.urlopen(twitcleanurl)
-            twitsoup=BeautifulSoup(twitpage.read())
-            twitsocialdata=twitsoup.findAll('a',{'data-nav':'followers'})
-#                print k, twitsociallink[0].findAll(text=True)[1], twitsociallink[0].findAll(text=True)[2]
-            twitfollowers = [f.findAll(text=True) for f in twitsocialdata]
-            twitsorteddata = sorted(twitfollowers)
-            twitterdb.twitter_followers = twitsorteddata[0][1]
-            twitterdb.save()
+                twitpage=urllib2.urlopen(twitcleanurl)
+                twitsoup=BeautifulSoup(twitpage.read())
+                twitsocialdata=twitsoup.findAll('a',{'data-nav':'followers'})
+    #                print k, twitsociallink[0].findAll(text=True)[1], twitsociallink[0].findAll(text=True)[2]
+                twitfollowers = [f.findAll(text=True) for f in twitsocialdata]
+                twitsorteddata = sorted(twitfollowers)
+                twitterdb.twitter_followers = twitsorteddata[0][1]
+                twitterdb.save()
 
-            print 'User: {0}, Followers: {1} Url: {2}'.format(user.title, twitsorteddata[0][1], twitcleanurl)
+                print 'User: {0}, Followers: {1} Url: {2}'.format(user.title, twitsorteddata[0][1], twitcleanurl)
+            except urllib2.HTTPError, e:
+                twitterdb.twitter_error = 'FB Page not found: {0}'.format(e)
+                twitterdb.save()
+                print 'User: {0}, FB Page not found: {1}'.format(user.title, e)
+            except urllib2.URLError, e:
+                twitterdb.twitter_error = 'URL Error: {0}'.format(e)
+                twitterdb.save()
+                print 'User: {0}, URL Error: {1}'.format(user.title, e)
+
                 
         print 'Done Pulling Twitter Followers'
 
