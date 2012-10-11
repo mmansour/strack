d4f1e2eb847d85e4d3f917ec3027fd50e87f8bbb
Matt Mansour <slackbabbath@gmail.com>
2012-10-11 15:30:27 -0700

    Cleaning up url sanitizer

--- trackit/management/commands/get_facebook.py
+++ trackit/management/commands/get_facebook.py
@@ -9,7 +9,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         yt = Youtuber.objects.exclude(facebook_url__isnull=True).exclude(facebook_url__exact='')
-        clean_url = ""
         
 #        for user in yt:
 ############ TEST URL CLEAN UP
@@ -35,31 +34,30 @@ class Command(BaseCommand):
 #                    clean_url = 'http://{0}'.format(user.facebook_url)
 #                else:
 #                    clean_url = user.facebook_url
+                clean_url = user.facebook_url.lower()
+
                 if user.facebook_url[:4] != 'http':
                     clean_url = 'http://{0}'.format(user.facebook_url)
-                elif user.facebook_url[:5] == 'https':
+                if user.facebook_url[:5] == 'https':
                     clean_url = user.facebook_url.replace('https','http')
-                elif user.facebook_url[:11] == 'http://face':
+                if user.facebook_url[:11] == 'http://face':
                     clean_url = user.facebook_url.replace('http://face','http://www.face')
-                else:
-                    clean_url = user.facebook_url
 
-                sanitized_url = clean_url.lower()
                 try:
-                    print 'Clean fb url %s' % sanitized_url
+                    print 'Clean fb url %s' % clean_url
                 except UnicodeEncodeError, e:
                     pass
 
-                fbpage=urllib2.urlopen(sanitized_url)
+                fbpage=urllib2.urlopen(clean_url)
                 fbsoup=BeautifulSoup(fbpage.read())
                 fbsociallink=fbsoup.findAll('meta',{'name':'description'})
                 fbdigitlist = [s for s in fbsociallink[0]['content'].split()
                                     if s.replace(",","").isdigit()]
     #                        print k, fbdigitlist, fbsociallink[0]['content']
                 fburldb.facebook_likes = fbdigitlist[0]
-                fburldb.facebook_url = sanitized_url
+                fburldb.facebook_url = clean_url
                 fburldb.save()
-                print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], sanitized_url)
+                print 'User: {0}, Likes: {1} Url: {2}'.format(user.title, fbdigitlist[0], clean_url)
             except IndexError, e:
                 fburldb.facebook_error = 'Missing Likes: {0}'.format(e)
                 fburldb.save()
